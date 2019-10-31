6216f8612fb1997159af67d346b99f2afea2c3d2
Alessandra <afleck@andrew.cmu.edu>
2019-10-31 14:47:52 -0400

    git commit

--- cart.js
+++ cart.js
@@ -1,20 +1,89 @@
 
-function cart(){
-	var numberofitems = document.getitemslogged('item-number-cart')
-	for (var i=0; i< numberofitems.length; i++){
-		var input = numberofitems[i]
-		input.addamount('total', newtotal)
-	}
-	var addToCart = document.getitemslogged('image-clicked')
-	for(var i=0, i<addToCart.length; i++){
-		var plus = addToCart[i]
-		plus.addToCart('select', addedToCart)
-	}
+*/When the order is complete and the user is ready to make decisions on the purchase /*
+
+*/Remove Items from the cart by taking them and adding the request to the removeCartItem array /*
+function complete() {
+    var removeCartItem = document.getElementsByClassName('remove-button')
+    for (var i = 0; i < removeCartItem.length; i++) {
+        var button = removeCartItem[i]
+        button.addEventListener('click', removeCartItem)
+    }
+*/Add items to the cart by adding them to the array /*
+    var addToCart = document.getElementsByClassName('shop-button')
+    for (var i = 0; i < addToCart.length; i++) {
+        var button = addToCart[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    var quantityInputs = document.getElementsByClassName('item-quantity')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    document.getElementsByClassName('shop-button')[0].addEventListener('click', purchaseConfirmed)
 }
 
-function addedToCart {
-	var plus = event.target
-	var imageSrc = img.imageSrc
-	addedToCart (number of clicks, imageSrc)
-	
+*/Creating events for each of the actions, as a result of the the action above the following result will take place /*
+
+*/Actually remove item and update the cart-total /*
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
 }
+
+*/A image of the item with the titel and price should be added to the cart -- the function for adding is below /*
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+*/Note that the quantity of items has changed and log it /*
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('item-title')
+    }
+
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('item-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('item-quantity')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('total-price')[0].innerText = '$' + total
+}
+
+function purchaseConfirmed() {
+    alert('Your buns are on their way!')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
